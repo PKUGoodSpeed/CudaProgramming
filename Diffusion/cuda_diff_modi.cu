e8a673b23230a4037bd9b584ef749931d96f47b0
PKUItachi <zebo@ginkgo-llc.com>
2017-09-26 18:16:32 +0000

    sfa

--- Diffusion/cuda_diff_modi.cu
+++ Diffusion/cuda_diff_modi.cu
@@ -4,7 +4,8 @@ using namespace std;
 const double pi = 3.14159265358979323846264;
 const double L = 100.;
 const double Diff = 1.;
-const int MAX_CELL_SIZE = 20;
+const int MAX_BLOCK_SIZE = 32;
+
 // In this method, we use squre cells of threads, but we need to specify the size of the square.
 
 /*
@@ -25,15 +26,15 @@ inline double analytical(double x,double y){
 }
 
 __global__ void oneIteration(int N, int cell_size, double *cur, double *old, double delta_x,double delta_t){
-    
+    // Run one step of iteration with multiple blocks of threads
     int b_size = blockDim.x, b_id = blockIdx.x, t_id = threadIdx.x, in_cell = cell_size - 2;
     int n_cell = (N + in_cell -2)/in_cell;
     
     assert(N > 1);
-    assert(cell_size <= MAX_CELL_SIZE);
+    assert(cell_size <= MAX_BLOCK_SIZE);
     assert(b_size == cell_size*cell_size);  // Here we only use square cells
     
-    __shared__ double tmp_arr[MAX_CELL_SIZE][MAX_CELL_SIZE];  // Define a buffer in the shared memory of one block of threads
+    __shared__ double tmp_arr[MAX_BLOCK_SIZE][MAX_BLOCK_SIZE];  // Define a buffer in the shared memory of one block of threads
     int dX[8] = {0, 0, 1, -1, 1, -1, 1, -1};
     int dY[8] = {1, -1, 0, 0, 1, 1, -1, -1};
     
@@ -49,7 +50,7 @@ __global__ void oneIteration(int N, int cell_size, double *cur, double *old, dou
     
     // Compute the updated value and store it into *cur
     if(local_i && local_i<cell_size-1 && local_j && local_j<cell_size-1 && global_i && global_i<N && globel_j && globel_j<N){
-        double nn_diff = 0;
+        double nn_diff = 0.;
         for(int k=0;k<8;++k) nn_diff += tmp_arr[local_i+dX[k]][local_j+dY[k]];
         nn_diff -= 8.*tmp_arr[local_i][local_j];
         cur[global_i*(N+1) + globel_j] = tmp_arr[local_i][local_j] + Diff*delta_t*nn_diff/3./pow(delta_x, 2.);
@@ -62,15 +63,39 @@ __global__ void oneIteration(int N, int cell_size, double *cur, double *old, dou
     return;
 }
 
+__global__ void iterationWithOneBlock(int N,int N_step, int t_width, double *cur, double *tmp, double delta_x, double delta_t){
+    // Run multiple steps of iterations with one block of threads
+    int t_id = threadIdx.x, b_size = blockDim.x, nx_thread = (N-2+t_width)/t_width;
+    assert(b_size == nx_thread*nx_thread);
+
+	// Define neighbor vectors:
+	int dX[8] = {0, 0, 1, -1, 1, -1, 1, -1};
+	int dY[8] = {1, -1, 0, 0, 1, 1, -1, -1};
+    int global_i = (t_id/nx_thread)*t_width, globel_j = (t_id%nx_thread)*t_width;
+    for(int step=0;step<N_step;++step){
+        for(int i=global_i;i<global_i+t_width;++i) for(int j=globel_j;j<globel_j+t_width;++j){
+            if(i && i<N && j && j<N){
+                double nn_sum = 0., coeff = Diff*delta_t/3./pow(delta_x, 2.);
+                for(int k=0;k<8;++k) nn_sum += cur[(i+dX[k])*(N+1) + j+dY[k]];
+                tmp[i*(N+1) + j] = (1.-8.*coeff)*cur[i*(N+1) + j] + coeff*nn_sum;
+            }
+        }
+        __syncthreads();
+        for(int i=global_i;i<global_i+t_width;++i) for(int j=globel_j;j<globel_j+t_width;++j){
+            if(i && i<N && j && j<N) cur[i*(N+1) + j] = tmp[i*(N+1) + j];
+        }
+        __syncthreads();
+    }
+    return;
+}
+
 class DiffEqnSolver{
     int n_grid, array_size, in_cell, cell_size, n_cell;
+    int t_width, nx_thread;
     double d_x, **val, *cur, *old;
 public:
-    DiffEqnSolver(int N, int c_size):n_grid(N), cell_size(c_size){
-        assert(cell_size > 2 && cell_size <= MAX_CELL_SIZE);
+    DiffEqnSolver(int N):n_grid(N){
         d_x = L/n_grid;
-        in_cell = cell_size - 2;
-        n_cell = (n_grid + in_cell - 2)/in_cell;
         array_size = (n_grid+1)*(n_grid+1);
         val = new double* [n_grid + 1];
         val[0] = new double [array_size];
@@ -98,15 +123,36 @@ public:
         return sqrt(sum);
     }
     
-    // One step of iteration
-    void oneStep( double d_t){
+    // Get grid size and block size
+    void setUpGrid(int c_size){
+        cell_size = c_size;
+        assert(cell_size > 2 && cell_size <= MAX_BLOCK_SIZE);
+        in_cell = cell_size - 2;
+        n_cell = (n_grid + in_cell - 2)/in_cell;
+    }
+    // One step of iteration with multiple blocks
+    void oneStep(double d_t){
         cudaMemcpy(old, val[0], array_size*sizeof(double), cudaMemcpyHostToDevice);
         oneIteration<<<n_cell*n_cell, cell_size*cell_size>>>(n_grid, cell_size, cur, old, d_x, d_t);
         cudaMemcpy(val[0], cur, array_size*sizeof(double), cudaMemcpyDeviceToHost);
     }
-    
+    // Run multiple iterations with multiple blocks
     double runIterations(int N_step, double d_t){
         for(int t=0;t<N_step;++t) oneStep(d_t);
+        return getError();
+    }
+    
+    // Get block size if we use only one block of threads
+    void setUpBlock(int nx_t){
+        nx_thread = nx_t;
+        assert(nx_thread > 0 && nx_thread <= MAX_BLOCK_SIZE);
+        t_width = (n_grid-2+nx_thread)/nx_thread;
+    }
+    // Run multiple iterations with only one block of threads
+    double runWithOneBlock(int N_step, double d_t){
+        cudaMemcpy(cur, val[0], array_size*sizeof(double), cudaMemcpyHostToDevice);
+        iterationWithOneBlock<<<1, nx_thread*nx_thread>>>(n_grid, N_step, t_width, cur, old, d_x, d_t);
+        cudaMemcpy(val[0], cur, array_size*sizeof(double), cudaMemcpyDeviceToHost);
         return getError();
     }
     
@@ -118,12 +164,19 @@ public:
     }
 };
 
-int main(){
-    DiffEqnSolver solver(100, 10);
+int main(int argc, char *argv[]){
+    DiffEqnSolver solver(100);
     solver.init(1.);
     int n_batch = 10, n_step = 1000;
     double dt = 0.5;
     cout<<setprecision(3);
+    solver.setUpGrid(16);
+    cout<<"Start running iterations:"<<endl;
+    clock_t start_time = clock(), end_time;
     for(int i=1;i<=n_batch;++i) cout<<"Iteration: "<<i<<"\t error:"<<solver.runIterations(n_step, dt)<<endl;
+    end_time = clock();
+    cout<<"End running iterations!"<<endl<<endl;
+    cout<<"Time spent during iterations: "<<double(end_time-start_time)/CLOCKS_PER_SEC<<"s\n\n\n";
+	cout<<"================================================================================"<<endl;
     return 0;
 }
