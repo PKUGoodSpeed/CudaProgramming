281c4984671cbd850546d98bc32662d5b4f6640f
PKUGoodSpeed <pkulizebo@gmail.com>
2017-09-26 02:03:52 -0700

    Sdf

--- Diffusion/cuda_diff_eqn.cpp
+++ Diffusion/cuda_diff_eqn.cpp
@@ -22,16 +22,6 @@ inline double analytical(double x,double y){
     return sinh(y*pi/L)*sin(x*pi/L);
 }
 
-__global__ void updateOld(int N, double *cur, double *old){
-    int idx = blockIdx.x*blockDim.x + threadIdx.x;
-    assert(N > 1);
-    if(idx < (N-1)*(N-1)){
-        int i = idx/(N-1) + 1, j = idx%(N-1) + 1;
-        old[i*(N-1) + j] = cur[i*(N-1) + j];
-    }
-    return;
-}
-
 __global__ void oneIteration(int N, double *cur, double *old, double delta_x,double delta_t){
     int idx = blockIdx.x*blockDim.x + threadIdx.x;
     int dX[8] = {0, 0, 1, -1, 1, -1, 1, -1};
@@ -67,26 +57,27 @@ public:
         }
     }
     void init(double init_val){
+        // Initialize the grid
         for(int i=1;i<n_grid;++i) for(int j=1;j<n_grid;++j) val[i][j] = init_val;
-        cudaMemcpy(cur, val[0], array_size*sizeof(double), cudaMemcpyHostToDevice);
     }
     
     double getError(){
+        // Using L2 norm
         double sum = 0.;
         for(int i=0;i<=n_grid;++i) for(int j=0;j<=n_grid;++j)
             sum += pow(val[i][j] - analytical(i*d_x, j*d_x),2.);
         return sqrt(sum);
     }
     
-    void oneStep( double d_t, bool useMemcpy = false){
-        if(useMemcpy) cudaMemcpy(old, cur, array_size*sizeof(double), cudaMemcpyDeviceToDevice);
-        else updateOld<<<n_block, block_size>>>(n_grid, cur, old);
+    void oneStep( double d_t){
+        cudaMemcpy(old, val[0], array_size*sizeof(double), cudaMemcpyHostToDevice);
+        cudaMemcpy(cur, val[0], array_size*sizeof(double), cudaMemcpyHostToDevice);
         oneIteration<<<n_block, block_size>>>(n_grid, cur, old, d_x, d_t);
+        cudaMemcpy(val[0], cur, array_size*sizeof(double), cudaMemcpyDeviceToToDevice);
     }
     
-    double runIterations(int N_step, double d_t, bool useMemcpy = false){
-        for(int t=0;t<N_step;++t) oneStep(d_t, useMemcpy);
-        cudaMemcpy(val[0], cur, array_size*sizeof(double), cudaMemcpyDeviceToHost);
+    double runIterations(int N_step, double d_t){
+        for(int t=0;t<N_step;++t) oneStep(d_t);
         return getError();
     }
     
@@ -104,7 +95,6 @@ int main(){
     int n_batch = 10, n_step = 1000;
     double dt = 0.5;
     cout<<setprecision(3);
-    for(int i=1;i<=n_batch;++i) cout<<"Iteration: "<<i<<"\t error:"<<solver.runIterations(n_step, dt, false)<<endl;
-    //for(int i=1;i<=n_batch;++i) cout<<"Iteration: "<<i<<"\t error:"<<solver.runIterations(n_step, dt, true)<<endl;
+    for(int i=1;i<=n_batch;++i) cout<<"Iteration: "<<i<<"\t error:"<<solver.runIterations(n_step, dt)<<endl;
     return 0;
 }
