011555bab062b38b1c83ca3b3ce5efdb1459a160
PKUItachi <zebo@ginkgo-llc.com>
2017-09-29 02:21:42 +0000

    sdf

--- VecAdd/inner.cu
+++ VecAdd/inner.cu
@@ -5,29 +5,23 @@ class TryClass{
 public:
     __global__ void vec_add(int N, int *A, int *B, int *C){
         int i = threadIdx.x + blockIdx.x * blockDim.x;
-        // assert( i<N );
-        if(i < N) C[i] = A[i] + B[i];
+        if(i < N) C[0] = A[i] * B[i];
     }
 };
 
 int main(int argc, char *argv[]){
-    if(argc <= 1){
-        cout<<"Error: No Input Dimension"<<endl;
-        return 0;
-    }
+    int N = 10, block_size = 16;
     srand(0);
-    int N = stoi(argv[1]), block_size = 256;
+    if(argc > 1) N = stoi(argv[1]);
     int n_block = (N+block_size-1)/block_size;
-    int *A = new int [N], *B = new int [N], *C = new int [N];
-    for(int i=0;i<N;++i) A[i] = rand()%50;
-    for(int i=0;i<N;++i) B[i] = rand()%50;
-    clock_t start_time, mid_time1, mid_time2, end_time;
-    // Record the starting time
-    start_time = clock();
+    int *A = new int [N], *B = new int [N], *C = new int [1];
+    for(int i=0;i<N;++i) A[i] = 1;
+    for(int i=0;i<N;++i) B[i] = 1;
+   
     int *dA, *dB, *dC;
     cudaMalloc((void **)&dA, N*sizeof(int));
     cudaMalloc((void **)&dB, N*sizeof(int));
-    cudaMalloc((void **)&dC, N*sizeof(int));
+    :cudaMalloc((void **)&dC, N*sizeof(int));
 
     // Copy data to divice
     cudaMemcpy(dA, A, N*sizeof(int), cudaMemcpyHostToDevice);
