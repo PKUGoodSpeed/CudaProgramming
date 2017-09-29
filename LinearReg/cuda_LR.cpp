d836ae75b70787a587e72e2a2d25a39209c38ed0
PKUGoodSpeed <pkulizebo@gmail.com>
2017-09-29 11:33:43 -0700

    default messages

--- LinearReg/cuda_LR.cpp
+++ LinearReg/cuda_LR.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long double ld;
+typedef long float ld;
 typedef vector<int> vi;
 typedef vector<long> vl;
 typedef vector<bool> vb;
-typedef vector<double> vd;
+typedef vector<float> vd;
 typedef pair<int,int> ii;
 typedef pair<long, long> ll;
 typedef unordered_set<int> ui;
@@ -14,12 +14,12 @@ const int MAX_BLOCK_SIZE  = 1024;
 const int MAX_NUM_FEATURES = 32;
 const int MAX_CASE_PER_THREAD = 8;
 
-__global__ void cudaUpdateWeight(int N, int K, int N_step, double l_rate, double *X, double *Y, double *new_w, double *old_w, int npt = 1){
+__global__ void cudaUpdateWeight(int N, int K, int N_step, float l_rate, float *X, float *Y, float *new_w, float *old_w, int npt = 1){
     // Naive way to do updates
     assert(blockDim.x <= MAX_BLOCK_SIZE);
     assert(npt <= MAX_CASE_PER_THREAD);
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    double X_tmp[MAX_CASE_PER_THREAD][MAX_NUM_FEATURES], Y_pred[MAX_CASE_PER_THREAD], Y_true[MAX_CASE_PER_THREAD];
+    float X_tmp[MAX_CASE_PER_THREAD][MAX_NUM_FEATURES], Y_pred[MAX_CASE_PER_THREAD], Y_true[MAX_CASE_PER_THREAD];
     int start = idx*npt, end = min(N, (idx+1)*npt);
     if(start >= N) return;
     for(int i=0;i<end-start;++i){
@@ -31,10 +31,10 @@ __global__ void cudaUpdateWeight(int N, int K, int N_step, double l_rate, double
     for(int step = 0; step < N_step; ++step){
         for(int i=0;i<end-start;++i){
             Y_pred[i] = 0.;
-            for(int j=0;j<K;++j) Y_pred += X_tmp[i][j]*old_w[j];
+            for(int j=0;j<K;++j) Y_pred[i] += X_tmp[i][j]*old_w[j];
         }
         for(int j = 0; j < K; ++j) {
-            double additive = 0.;
+            float additive = 0.;
             for(int i=0;i<end-start;++i) additive += (Y_true[i] - Y_pred[i])*X_tmp[i][j];
             additive *= l_rate/N;
             atomicAdd(&new_w[j], additive);
@@ -44,15 +44,15 @@ __global__ void cudaUpdateWeight(int N, int K, int N_step, double l_rate, double
     return;
 }
 
-__global__ void cudaGetError(int N, int K, double *X, double *Y, double *weights, double *dev_err, int npt = 1){
+__global__ void cudaGetError(int N, int K, float *X, float *Y, float *weights, float *dev_err, int npt = 1){
     assert(blockDim.x <= MAX_BLOCK_SIZE);
     assert(npt <= MAX_CASE_PER_THREAD);
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     int start = idx*npt, end = min(N, (idx+1)*npt);
     if(start >= N) return;
-    double blk_sum = 0.;
+    float blk_sum = 0.;
     for(int i=start; i<end ; ++i){
-        double diff = -Y[i];
+        float diff = -Y[i];
         for(int j=0;j<K;++j) diff += weights[j] * X[i*K + j];
         blk_sum += diff * diff;
     }
@@ -67,49 +67,49 @@ class CudaLinearReg{
     int N_train, N_test, N_feat;
     int B_size, N_block;
     // In this case, for convenient, consider weights and bias together.
-    double **X_train, *Y_train, **X_test, *Y_test, *weights;
-    double *dev_X, *dev_Y, *dev_w, *dev_old_w, *dev_err;
-    inline double getRandNum(){ return double(rand())/RAND_MAX; }
+    float **X_train, *Y_train, **X_test, *Y_test, *weights;
+    float *dev_X, *dev_Y, *dev_w, *dev_old_w, *dev_err;
+    inline float getRandNum(){ return float(rand())/RAND_MAX; }
 public:
     CudaLinearReg(int n_trian, int n_test, int n_feat, int block_size = 0):N_train(n_trian), N_test(n_test), N_feat(n_feat){
         
         // Create serial storage for X_train
-        X_train = new double* [N_train];
-        X_train[0] = new double [N_train * N_feat];
+        X_train = new float* [N_train];
+        X_train[0] = new float [N_train * N_feat];
         for(int i=1;i<N_train;++i) X_train[i] = X_train[i-1] + N_feat;
         
         // Create serial storage for Y_train
-        Y_train = new double [N_train];
+        Y_train = new float [N_train];
         
         // Create serial storage for X_test
-        X_test = new double* [N_tes];
-        X_test[0] = new double [N_test * N_feat];
+        X_test = new float* [N_tes];
+        X_test[0] = new float [N_test * N_feat];
         for(int i=1;i<N_test;++i) X_test[i] = X_test[i-1] + N_feat;
         
         // Create serial storage for Y_test
-        Y_test = new double [N_test];
+        Y_test = new float [N_test];
         
         // Create serial storage for weights
-        weights = new double [N_feat];
+        weights = new float [N_feat];
         
         // Create memory for X, Y, weights, old_weights on GPU device
-        cudaMalloc((void **)&dev_X, N_train*N_feat*sizeof(double));
-        cudaMalloc((void **)&dev_Y, N_train*sizeof(double));
-        cudaMalloc((void **)&dev_w, N_feat*sizeof(double));
-        cudaMalloc((void **)&dev_old_w, N_feat*sizeof(double));
-        cudaMalloc((void **)&dev_err, 1*sizeof(double));
+        cudaMalloc((void **)&dev_X, N_train*N_feat*sizeof(float));
+        cudaMalloc((void **)&dev_Y, N_train*sizeof(float));
+        cudaMalloc((void **)&dev_w, N_feat*sizeof(float));
+        cudaMalloc((void **)&dev_old_w, N_feat*sizeof(float));
+        cudaMalloc((void **)&dev_err, 1*sizeof(float));
         
         if(block_size <= 0 || block_size>MAX_BLOCK_SIZE) B_size = min(MAX_BLOCK_SIZE, N_train);
         else B_size = block_size;
         N_block = (N_train + B_size - 1)/B_size;
     }
     
-    void loadData(double **trX, double *trY, double **teX, double *teY){
+    void loadData(float **trX, float *trY, float **teX, float *teY){
         // Make sure that all the dimensions are correct
-        memcpy(X_train[0], trX[0], N_train*N_feat*sizeof(double));
-        memcpy(Y_train, trY, N_train*sizeof(double));
-        memcpy(X_test[0], teX[0], N_test*N_feat*sizeof(double));
-        memcpy(Y_test, teY, N_test*sizeof(double));
+        memcpy(X_train[0], trX[0], N_train*N_feat*sizeof(float));
+        memcpy(Y_train, trY, N_train*sizeof(float));
+        memcpy(X_test[0], teX[0], N_test*N_feat*sizeof(float));
+        memcpy(Y_test, teY, N_test*sizeof(float));
     }
     
     void setBlocks(int block_size){
@@ -118,45 +118,45 @@ public:
         N_block = (N_train + B_size - 1)/B_size;
     }
     
-    void initWeights(double amp_weight = 2.0){
+    void initWeights(float amp_weight = 2.0){
         // Initializing weights
         for(int i=0;i<N_feat;++i) weights[i] = getRandNum();
     }
     
     void initGPU(){
         // Initializing CUDA memories
-        cudaMemcpy(dev_X, train_X[0], N_train*N_feat*sizeof(double), cudaMemcpyHostToDevice);
-        cudaMemcpy(dev_Y, train_Y, N_train*sizeof(double), cudaMemcpyHostToDevice);
-        cudaMemcpy(dev_w, weights, N_feat*sizeof(double), cudaMemcpyHostToDevice);
-        cudaMemcpy(dev_old_w, weights, N_feat*sizeof(double), cudaMemcpyHostToDevice);
+        cudaMemcpy(dev_X, train_X[0], N_train*N_feat*sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(dev_Y, train_Y, N_train*sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(dev_w, weights, N_feat*sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(dev_old_w, weights, N_feat*sizeof(float), cudaMemcpyHostToDevice);
     }
     
-    double getError(bool isTest = false, int npt = 1){
+    float getError(bool isTest = false, int npt = 1){
         // We use mean squre root error here.
         // Here we use the vector pred_Y to record the predicted value
-        double error = 0;
+        float error = 0;
         int N = N_train;
-        cudaMemcpy(dev_err, &error, 1*sizeof(double), cudaMemcpyHostToDevice);
-        cudaMemcpy(dev_w, weights, N_feat*sizeof(double), cudaMemcpyHostToDevice);
+        cudaMemcpy(dev_err, &error, 1*sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(dev_w, weights, N_feat*sizeof(float), cudaMemcpyHostToDevice);
         if(isTest){
             N = N_test;
-            cudaMemcpy(dev_X, test_X[0], N*N_feat*sizeof(double), cudaMemcpyHostToDevice);
-            cudaMemcpy(dev_Y, test_Y, N*sizeof(double), cudaMemcpyHostToDevice);
+            cudaMemcpy(dev_X, test_X[0], N*N_feat*sizeof(float), cudaMemcpyHostToDevice);
+            cudaMemcpy(dev_Y, test_Y, N*sizeof(float), cudaMemcpyHostToDevice);
         }
         else{
-            cudaMemcpy(dev_X, train_X[0], N*N_feat*sizeof(double), cudaMemcpyHostToDevice);
-            cudaMemcpy(dev_Y, train_Y, N*sizeof(double), cudaMemcpyHostToDevice);
+            cudaMemcpy(dev_X, train_X[0], N*N_feat*sizeof(float), cudaMemcpyHostToDevice);
+            cudaMemcpy(dev_Y, train_Y, N*sizeof(float), cudaMemcpyHostToDevice);
         }
         cudaGetError<<<N_block, B_size>>>(N, N_feat, dev_X, dev_Y, dev_w, dev_err, npt);
-        cudaMemcpy(&error, dev_err, 1*sizeof(double), cudaMemcpyDeviceToHost);
+        cudaMemcpy(&error, dev_err, 1*sizeof(float), cudaMemcpyDeviceToHost);
         return error/N;
     }
     
-    void cudaNaiveTrain(int N_step, double learning_rate, int npt = 1;){
+    void cudaNaiveTrain(int N_step, float learning_rate, int npt = 1;){
         // Call the GPU update, which uses the Naive approach.
         initGPU();
         cudaUpdateWeight<<<N_block, B_size>>>(N_train, N_feat, N_step, learning_rate, dev_X, dev_Y, dev_w, dev_old_w, npt);
-        cudaMemcpy(weights, dev_w, N_feat*sizeof(double), cudaMemcpyDeviceToHost);
+        cudaMemcpy(weights, dev_w, N_feat*sizeof(float), cudaMemcpyDeviceToHost);
     }
     
     vd getWeights(){
@@ -182,25 +182,25 @@ public:
 };
 
 class TestLinearReg{
-    double ampli, **train_x, *train_y, **test_x, *test_y;
+    float ampli, **train_x, *train_y, **test_x, *test_y;
     int N_train, N_test, N_feat;
     vd weights;
-    double linearFn(double *x){
-        double ans = 0.;
+    float linearFn(float *x){
+        float ans = 0.;
         for(int i=0;i<N_feat;++i) ans += x[i] * weights[i];
         return ans;
     }
-    double quardFn(const vd &x){
+    float quardFn(const vd &x){
         assert(weights.size() == 2*x.size());
-        double ans = bias;
+        float ans = bias;
         for(int i=0;i<n_var;++i){
             ans += weights[2*i]*x[i] + weights[2*i+1]*x[i]*x[i];
         }
         return ans;
     }
-    inline double getRandNum(){ return double(rand())/RAND_MAX; }
+    inline float getRandNum(){ return float(rand())/RAND_MAX; }
 public:
-    TestLinearReg(vd correct_w, int n_tr, int n_te, double Amp = 0.2): weights(correct_w), N_train(n_tr), N_test(n_te), ampli(Amp){
+    TestLinearReg(vd correct_w, int n_tr, int n_te, float Amp = 0.2): weights(correct_w), N_train(n_tr), N_test(n_te), ampli(Amp){
         srand(1);
         N_feat = (int)weights.size();
         assert(N_feat > 1);
@@ -208,15 +208,15 @@ public:
         
         
         // Allocate memories
-        train_x = new double* [N_train];
-        train_x[0] = new double [N_train*N_feat];
+        train_x = new float* [N_train];
+        train_x[0] = new float [N_train*N_feat];
         for(int i=1;i<N_train;++i) train_x[i] = train_x[i-1] + N_feat;
-        train_y = new double [N_train];
+        train_y = new float [N_train];
         
-        test_x = new double* [N_test];
-        test_x[0] = new double [N_test*N_feat];
+        test_x = new float* [N_test];
+        test_x[0] = new float [N_test*N_feat];
         for(int i=1;i<N_test;++i) test_x[i] = test_x[i-1] + N_feat;
-        test_y = new double [N_test];
+        test_y = new float [N_test];
         
         // Show something on the screen:
         cerr << setprecision(6);
@@ -260,10 +260,10 @@ public:
         }
     }
     /*
-    vector<vd> testModel(double l_rate,int n_block,int n_step){
+    vector<vd> testModel(float l_rate,int n_block,int n_step){
         vector<vd> ans(3, vd());
         LinearReg model(train_x, train_y, test_x, test_y);
-        double steps = 0.;
+        float steps = 0.;
         ans[0].push_back(steps);
         model.computePred();
         ans[1].push_back(model.getError());
@@ -274,7 +274,7 @@ public:
             ans[1].push_back(model.getTestError());
         }
         vd pred_wei = model.getWeights();
-        double pred_bias = model.getBias();
+        float pred_bias = model.getBias();
         cerr<<"Here is what we obtain: y = x1*"<<pred_wei[0]<<" + x2*"<<pred_wei[1]<<" +"<<pred_bias<<endl;
         return ans;
     }*/
@@ -290,7 +290,7 @@ public:
 
 int main(int argc, char* argv[]){
     std::ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
-    double w1 = 1.7, w2 = 0.8, b = 2.2;
+    float w1 = 1.7, w2 = 0.8, b = 2.2;
     int n_train = 7000, n_test = 3000;
     if(argc > 1) w1 = stod(argv[1]);
     if(argc > 2) w2 = stod(argv[2]);
