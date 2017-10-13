70c6ec0b7c9f2261adb34dc9a347bd5895b4f48c
PKUItachi <zebo@ginkgo-llc.com>
2017-10-13 22:02:38 +0000

    dsf

--- Template/test/main.cpp
+++ Template/test/main.cpp
@@ -7,7 +7,7 @@ int main(){
 	vector<int> A{1, 2, 3, 4, 5}, B{6, 7, 8, 9, 10};
 	cout << "CPU version:" << endl;
 	cout << InnerProd<cpu, int>()(A, B) << endl << endl;
-	//cout << "GPU version:" << endl;
-	//cout << InnerProd<gpu, int>()(A, B) << endl << endl;
+	cout << "GPU version:" << endl;
+	cout << InnerProd<gpu, int>()(A, B) << endl << endl;
 	return 0;
 }
