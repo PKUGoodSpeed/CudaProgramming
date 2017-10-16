073cf080dff96979b34c0f5602b6c15359b3de5b
Zebo <zebo@Zebos-MacBook-Pro.local>
2017-10-16 12:14:16 -0700

    Sdf

--- cuBLAS/Matrix/matmul/gpu_matrix.cu
+++ cuBLAS/Matrix/matmul/gpu_matrix.cu
@@ -17,5 +17,27 @@ vector<int> MatrixMultiplication<gpu, int>::operator ()(const vector<int> &A, co
 	clock_t t_end = clock();
 	cout<<"GPU Matrix Multiplication Time Usage:"<<endl;
 	cout<< double(t_end - t_start)/CLOCKS_PER_SEC << " s"<<endl;
-	return C;
+	return vector<int>();
+}
+
+int main(){
+	vector<int> A = {1,2,3,4,5,6}, B = {1,2,3,4,5,6};
+	int rA = 2, cB = 2, cA = 3, rB = 3;
+	cout<<"A:"<<endl;
+	for(int i=0;i<rA;++i){
+		for(int j=0;j<cA;++j) cout<<A[i*cA + j]<<' ';
+		cout<<endl;
+	}
+	cout<<"B:"<<endl;
+	for(int i=0;i<rB;++i){
+		for(int j=0;j<cB;++j) cout<<B[i*cB + j]<<' ';
+		cout<<endl;
+	}
+	vector<int> C = MatrixMultiplication<cpu, int>()(A, B, rA, cA, rB, cB);
+	cout<<"C:"<<endl;
+	for(int i=0;i<rA;++i){
+		for(int j=0;j<cB;++j) cout<<C[i*cB + j]<<' ';
+		cout<<endl;
+	}
+	return 0;
 }
