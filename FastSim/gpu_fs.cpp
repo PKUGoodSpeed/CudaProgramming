6afecf13354fd2b6a7c77f5e6ed37f1edce93342
Zebo <zebo@Zebos-MacBook-Pro.local>
2017-10-19 16:56:01 -0700

    add all

--- FastSim/gpu_fs.cpp
+++ FastSim/gpu_fs.cpp
@@ -72,6 +72,7 @@ void FastSim<gpu, double>::operator ()(const int &start_pos, const int &N_batch)
     thrust::copy(latencies.begin()+start_pos, latencies.begin()+start_pos+N_batch, dev_late.begin());
     
     // Doing parallelized fast simulation
+    cout<<"shaocong"<<endl;
     simKernel<<<(N_stgy + BLOCK_SIZE - 1)/BLOCK_SIZE, BLOCK_SIZE>>>(N_stgy, N_batch, to_ptr(dev_C), to_ptr(dev_mid), to_ptr(dev_gap),
                                                                     to_ptr(dev_late), to_ptr(dev_pos), to_ptr(dev_res), to_ptr(dev_prof), to_ptr(dev_prc));
     for(int i=0;i<N_stgy;++i){
