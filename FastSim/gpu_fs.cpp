1922478021fa53173025e3c22f42cbc5a2280476
Zebo <zebo@Zebos-MacBook-Pro.local>
2017-10-20 10:53:27 -0700

    add all

--- FastSim/gpu_fs.cpp
+++ FastSim/gpu_fs.cpp
@@ -1,8 +1,13 @@
 #undef _GLIBCXX_USE_INT128
 
 #include <iostream>
+#include <fstream>
 #include <cassert>
 #include <ctime>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
 #include <thrust/device_vector.h>
 #include <thrust/copy.h>
 #include <cublas_v2.h>
@@ -95,28 +100,50 @@ void FastSim<gpu, double>::fastSimulation(const vector<vector<double>> &weights,
     return;
 }
 
-int main(){
-    vector<vector<double>> signals = {
-        {1., 2., 3., -1., 1., -1., 1., -1.},
-        {1., 2., 3., -1., 1., -1., 1., -1.},
-        {1., 2., 3., -1., 1., -1., 1., -1.},
-        {1., 2., 3., -1., 1., -1., 1., -1.}
-    };
-    vector<vector<double>> prices = {
-        {3., 1., 4., 1., 5., 2., 1, 4.},
-        {9., 2., 6., 3., 7., 3., 2, 7.}
-    };
-    vector<vector<double>> weights = {
-        {1., 1., 0., 0.},
-        {0., 0., -1., 0.},
-        {0., 0., 0., -1.}
-    };
+int main(int argc, char *argv[]){
+    assert(argc > 1);
+    ifstream fin;
+    fin.open(argv[1]);
+    string info;
+    getline(fin, info);
+    int N_samp = 1E6, N_feat = 11, N_stgy = 400;
+    vector<vector<double>> prices(2, vector<double>(N_samp)), signals(N_feat, vector<double>(N_samp,0));
+    clock_t t_start = clock();
+    vector<int> late(N_samp, 5);
+    for(int i=0;i<N_samp;++i){
+        getline(fin, info);
+        auto j = info.find(',') + 1;
+        double mid = stod(info.substr(j));
+        j = info.find(',',j) + 1;
+        for(int k=0;k<11;++k){
+            signals[k][i] = stod(info.substr(j));
+            j = info.find(',',j) + 1;
+        }
+        double gap = stod(info.substr(j));
+        prices[0][i] = mid - gap/2.;
+        prices[1][i] = mid + gap/2.;
+    }
     FastSim<gpu, double> test(signals, prices);
-    vector<int> late = {1, 1, 1, 1, 1, 1, 1, 1};
+    clock_t t_end();
+    cout<<"Time usage for reading the data is "<<double(t_end - t_start)/CLOCKS_PER_SEC<<" s"<<endl<<endl;
+    
+    t_start = clock();
     auto res = test.getPerfectOps(late);
-    cout<<"The optimal operation list is: \n";
-    for(auto k:res) cout<<k<<' ';
-    cout<<endl;
-    test.fastSimulation(weights, late, 4);
+    t_end = clock();
+    cout<<"Time usage for computing perfect action list is "<<double(t_end - t_start)/CLOCKS_PER_SEC<<" s"<<endl<<endl;
+    
+    cout<<"Testing GPU fast sim performance:\n";
+    cout<<"Randomly generating weights from 0~1"<<endl;
+    t_start = clock();
+    vector<vector<double>> weights(N_stgy, vector<double>(N_feat));
+    for(int i=0;i<N_stgy;++i) generate(weights[i].begin(), weights[i].end(), [](){return (double)rand()/RAND_MAX;});
+    t_end = clock();
+    cout<<"Time usage for generating the weights is "<<double(t_end - t_start)/CLOCKS_PER_SEC<<" s"<<endl<<endl;
+    
+    t_start = clock();
+    test.fastSimulation(weights, late, N_samp);
+    t_end = clock();
+    cout<<"Time usage for gpu fast sim is "<<double(t_end - t_start)/CLOCKS_PER_SEC<<" s"<<endl<<endl;
+    
     return 0;
 }
