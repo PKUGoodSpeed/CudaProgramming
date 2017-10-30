f134fd47ac4227b1573ec611191df295e57100d5
Zebo Li <zebo@tahoe5>
2017-10-29 20:41:06 -0700

    sdf

--- MemExp/map_impl/complete_map/cpu_map.cpp
+++ MemExp/map_impl/complete_map/cpu_map.cpp
@@ -1,6 +1,16 @@
 /*
  We consider 1024 map, which are initially constructed with (at most)size = 4096 key, value pairs, for which keys are integers and values are float point numbers.
- Then each of them processes 262,144 operations, which only include 'g': search/getting values (If did not find, return -1.), 'e': check empty, 'z': check size.
+ Then each of them processes 262,144 operations. 
+ The operations include:
+ 'i': insert key, value pair. If the map is full, or already have this element, do not insert, return 0.
+ 'r': remove a key. If the map do not have that element, do nothing and retion 0.
+ 'm': modify the value for a particular key (which can be combined with insert). If that value do not exist, return 0. and do nothing.
+ 's': search whether there is a key.
+ 'g': extract the value for a particular key. If did not find, return 0., but do not insert.
+ 'z': return the size of the map.
+ 'e': return whether the map is empty or not.
+ 'f': return whether the map is full or not.
+ The above are symbols for the input operation list
  */
 #include <bits/stdc++.h>
 #include <cassert>
@@ -14,39 +24,60 @@ const int MOD = 10000;
 
 // CPU version
 class CPUMapTest{
-    vector<unordered_map<int, float>> maps;
+    vector<map<int, float>> maps;
     vector<char> ops;
-    vector<int> input;
+    vector<int> input_keys;
+    vector<float> input_values;
     int N_ops, N_ins;
 public:
-    CPUMapTest(const vector<vector<int>> &keys, const vector<vector<float>> &values): N_ins((int)keys.size()){
-        maps.resize(keys.size());
-        for(int i=0;i<(int)keys.size();++i){
-            for(int j=0;j<(int)keys[i].size();++j) maps[i][keys[i][j]] = values[i][j];
-        }
+    CPUMapTest(int num_ins): N_ins(num_ins){
+        maps.resize(N_ins);
     }
-    void loadOps(const vector<char> &ops, const vector<int> &input, int n_ops){
+    void loadOps(const vector<char> &ops, const vector<int> &input,const vector<float> &vals, int n_ops){
         N_ops = n_ops;
         assert((int)ops.size() == N_ops*N_ins);
         assert((int)input.size() == N_ops*N_ins);
         this->ops = ops;
-        this->input = input;
+        this->input_keys = input;
+        this->input_values = vals;
         return;
     }
     void procOps(vector<float> &ans){
         ans.resize(N_ops * N_ins);
         for(int i=0;i<N_ins;++i){
             for(int j=0;j<N_ops;++j){
-                if(ops[i*N_ops + j] == 'e'){
-                    ans[i*N_ops + j] = float(maps[i].empty());
+                char c = ops[i*N_ops + j];
+                int key = input_keys[i*N_ops + j];
+                float res = 0., value = input_values[i*N_ops + j];
+                if(c == 'i'){
+                    if((int)maps[i].size() < MAX_MAP_SIZE && !maps[i].count(key)){
+                        maps[i][key] = value;
+                        res = 1.;
+                    }
                 }
-                else if(ops[i*N_ops + j] == 'z'){
-                    ans[i*N_ops + j] = float(maps[i].size());
+                else if(c == 'r'){
+                    if(maps[i].count(key)){
+                        maps[i].erase(key);
+                        res = 1.;
+                    }
                 }
-                else{
-                    if(maps[i].count(input[i*N_ops + j])) ans[i*N_ops + j] =  maps[i][input[i*N_ops + j]];
-                    else ans[i*N_ops + j] = -1.;
+                else if(c == 'm'){
+                    if(maps[i].count(key)) {
+                        maps[i][key] = value;
+                        res = 1.;
+                    }
                 }
+                else if(c == 's') res = float(maps[i].count(key));
+                else if(c == 'g'){
+                    if(maps[i].count(key)) {
+                        res = maps[i][key];
+                    }
+                }
+                else if(c == 'z') res = float(maps[i].size());
+                else if(c == 'e') res = float(maps[i].empty());
+                else if(c == 'f') res = float((int)maps[i].size() == MAX_MAP_SIZE);
+                else res = 0.;
+                ans[i*N_ops + j] = res;
             }
         }
         return;
@@ -54,33 +85,28 @@ public:
 };
 
 int main(int argc, char *argv[]){
-    int size = MAX_MAP_SIZE, num_ins = NUM_INSTANCE, num_ops = NUM_OPERATION;
+    srand(0);
+    int num_ins = NUM_INSTANCE, num_ops = NUM_OPERATION;
     if(argc > 1) num_ins = stoi(argv[1]);
     if(argc > 2) num_ops = stoi(argv[2]);
     /* Generating data*/
     clock_t cpu_time = clock();
-    vector<vector<int>> keys(num_ins);
-    vector<vector<float>> values(num_ins);
-    vector<char> ops(num_ins * num_ops, 'g');
-    vector<int> input(num_ins * num_ops);
-    for(int i=0;i<num_ins;++i){
-        unordered_set<int> exist;
-        for(int j=0;j<size;++j){
-            int tmp_key = rand()%MOD;
-            if(!exist.count(tmp_key)){
-                keys[i].push_back(tmp_key);
-                values[i].push_back(float(rand())/RAND_MAX);
-                exist.insert(tmp_key);
-            }
-        }
-    }
-    generate(input.begin(), input.end(), [](){return rand()%MOD;});
+    string ref;
+    ref += string(500, 'g') + string(500, 's') + string(200, 'i') + string(200, 'm') + "e" + "zz" + "f" + string(100, 'r');
+    vector<char> ops(num_ins * num_ops);
+    vector<int> input_keys(num_ins * num_ops);
+    vector<float> input_values(num_ins * num_ops);
+    generate(input_keys.begin(), input_keys.end(), [](){return rand()%MOD;});
+    generate(input_values.begin(), input_values.end(), [](){return float(rand())/RAND_MAX;});
+    generate(ops.begin(), ops.end(), [&ref](){return ref[rand()%(int)ref.size()];});
     cout<<"Time Usage for generating random data is: "<<float(clock() - cpu_time)/CLOCKS_PER_SEC<<"s"<<endl;
     
+
     cpu_time = clock();
-    CPUMapTest cpu_test(keys, values);
-    cpu_test.loadOps(ops, input, num_ops);
+    CPUMapTest cpu_test(num_ins);
+    cpu_test.loadOps(ops, input_keys, input_values, num_ops);
     cout<<"Time Usage for preparing maps is: "<<float(clock() - cpu_time)/CLOCKS_PER_SEC<<"s"<<endl;
+    
     
     cpu_time = clock();
     vector<float> ans;
--- MemExp/map_impl/complete_map/gpu_map.cpp
+++ MemExp/map_impl/complete_map/gpu_map.cpp
@@ -1,11 +1,22 @@
 /*
  We consider 1024 map, which are initially constructed with (at most)size = 4096 key, value pairs, for which keys are integers and values are float point numbers.
- Then each of them processes 262,144 operations, which only include 'g': search/getting values (If did not find, return -1.), 'e': check empty, 'z': check size.
+ Then each of them processes 262,144 operations. 
+ The operations include:
+ 'i': insert key, value pair. If the map is full, or already have this element, do not insert, return 0.
+ 'r': remove a key. If the map do not have that element, do nothing and retion 0.
+ 'm': modify the value for a particular key (which can be combined with insert). If that value do not exist, return 0. and do nothing.
+ 's': search whether there is a key.
+ 'g': extract the value for a particular key. If did not find, return 0., but do not insert.
+ 'z': return the size of the map.
+ 'e': return whether the map is empty or not.
+ 'f': return whether the map is full or not.
+ The above are symbols for the input operation list
  */
 #include <bits/stdc++.h>
 #include <cassert>
 #include <thrust/device_vector.h>
 #include <thrust/copy.h>
+#include <thrust/execution_policy.h>
 #define to_ptr(x) thrust::raw_pointer_cast(&x[0])
 #define gpu_copy(x, y) thrust::copy((x).begin(), (x).end(), (y).begin())
 #define gpu_copy_to(x, y, pos) thrust::copy((x).begin(), (x).end(), (y).begin() + (pos))
@@ -13,74 +24,136 @@
 
 using namespace std;
 
-const int BLOCK_SIZE = 1024;
+const int BLOCK_SIZE = 1;
 
 const int NUM_INSTANCE = 1024;
 const int NUM_OPERATION = 262144;
 const int MAX_MAP_SIZE = 4096;
 const int MOD = 10000;
 
-__device__ float cuMapGetValue(int size, int *keys, float *values, int key){
-    for(int i=0;i<size;++i) if(keys[i] == key) return values[i];
-    return -1.;
+__device__ void cudaInsert(int *size, int *keys, float *values, int key, float value){
+    if((*size) == MAX_MAP_SIZE) return ;
+    int sz = *size;
+    int idx = int(thrust::find(thrust::device, keys, keys+sz, key) - keys);
+    if(idx < sz) return ;
+    keys[sz] = key;
+    values[sz] = value;
+    (*size) += 1;
+}
+
+__device__ void cudaRemove(int *size, int *keys, float *values, int key){
+    int sz = *size;
+    int idx = int(thrust::find(thrust::device, keys, keys+sz, key) - keys);
+    if(idx == sz) return;
+    keys[idx] = keys[sz - 1];
+    values[idx] = values[sz - 1];
+    (*size) -= 1;
+}
+
+__device__ void cudaModify(int size, int *keys, float *values, int key, float value){
+    int idx = int(thrust::find(thrust::device, keys, keys+size, key) - keys);
+    if(idx < size){
+        values[idx] = value;
+        keys[idx] = key;
+    }
+}
+
+__device__ bool cudaSearch(int size, int *keys, int key){
+    int idx = int(thrust::find(thrust::device, keys, keys+size, key) - keys);
+    return idx<size;
+}
+
+__device__ float cudaGetValue(int size, int *keys, float *values, int key){
+    int idx = int(thrust::find(thrust::device, keys, keys+size, key) - keys);
+    if(idx < size) return values[idx];
+    return 0.;
+}
+
+__device__ int cudaGetSize(int size){
+    return size;
+}
+
+__device__ bool cudaIsEmpty(int size){
+    return !size;
+}
+
+__device__ bool cudaIsFull(int size){
+    return size == MAX_MAP_SIZE;
 }
 
 /* 
 here we assume that the operation is only 'g', which is getting values.
 Currently we are only testing the case that do not need synchronization
 */
-__global__ void cudaProcKernel(int n_ins, int *sizes, int *keys, float *values, int n_ops, int *input, float *ans){
-    int b_sz = blockDim.x, b_idx = blockIdx.x, t_idx = threadIdx.x;
-    __shared__ int local_keys[MAX_MAP_SIZE];
-    __shared__ float local_values[MAX_MAP_SIZE];
-    int map_size = sizes[b_idx], map_start = MAX_MAP_SIZE * b_idx, ops_start = n_ops * b_idx;
-    assert(map_size <= MAX_MAP_SIZE);
-    for(int i=t_idx ; i<map_size ; i+=b_sz){
+__global__ void cudaProcKernel(int n_ins, int *sizes, int *keys, float *values, 
+int n_ops, char *ops, int *input_keys, float *input_values, float *ans){
+    int b_idx = blockIdx.x;
+    int local_keys[MAX_MAP_SIZE];
+    float local_values[MAX_MAP_SIZE];
+    int map_start = MAX_MAP_SIZE * b_idx, ops_start = n_ops * b_idx, size = sizes[b_idx];;
+    for(int i=0;i<size;++i){
         local_keys[i] = keys[map_start + i];
         local_values[i] = values[map_start + i];
     }
-    __syncthreads();
-    for(int i=t_idx;i<n_ops;i+=b_sz){
-        ans[ops_start + i] = cuMapGetValue(map_size, local_keys, local_values, input[ops_start + i]);
+    for(int i=0;i<n_ops;++i){
+        int j = ops_start + i;
+        char c = ops[j];
+        int key = input_keys[j];
+        float value = input_values[j];
+        if(c == 'i') cudaInsert(&size, local_keys, local_values, key, value);
+        else if(c == 'r') cudaRemove(&size, local_keys, local_values, key);
+        else if(c == 'm') cudaModify(size, local_keys, local_values, key, value);
+        else if(c == 's') ans[j] = (float)cudaSearch(size, local_keys, key);
+        else if(c == 'g') ans[j] = cudaGetValue(size, local_keys, local_values, key);
+        else if(c == 'z') ans[j] = (float)size;
+        else if(c == 'e') ans[j] = (float)(!size);
+        else if(c == 'f') ans[j] = (float)(size == MAX_MAP_SIZE);
+        else ans[j] = 0.;
     }
+    for(int i=0;i<size;++i){
+        keys[map_start + i] = local_keys[i];
+        values[map_start + i] = local_values[i];
+    }
+    sizes[b_idx] = size;
     return ;
 }
 
 // CPU version
 class GPUMapTest{
     int N_ins, N_ops;
-    def_dvec(int) dkeys, dinput, dsizes;
-    def_dvec(float) dvalues;
+    def_dvec(int) dkeys, dinkeys, dsizes;
+    def_dvec(float) dvalues, dinvalues;
+    def_dvec(char) dops;
 public:
-    GPUMapTest(const vector<vector<int>> &keys, const vector<vector<float>> &values): N_ins((int)keys.size()){
-        assert((int)values.size() == N_ins);
-        dkeys.resize(N_ins * MAX_MAP_SIZE);
-        dvalues.resize(N_ins * MAX_MAP_SIZE);
-        dsizes.resize(N_ins);
-        for(int i=0;i<N_ins;++i){
-            gpu_copy_to(keys[i], dkeys, i*MAX_MAP_SIZE);
-            gpu_copy_to(values[i], dvalues, i*MAX_MAP_SIZE);
-            dsizes[i] = (int)keys[i].size();
-        }
+    GPUMapTest(int num_ins): N_ins(num_ins){
+        dkeys.resize(num_ins * MAX_MAP_SIZE);
+        dvalues.resize(num_ins * MAX_MAP_SIZE);
+        dsizes.assign(num_ins, 0);
     }
-    void loadOps(const vector<int> &input, int n_ops){
+    void loadOps(const vector<char> &ops, const vector<int> &inkeys,const vector<float> &invals, int n_ops){
         N_ops = n_ops;
-        assert((int)input.size() == N_ops * N_ins);
-        dinput.resize(N_ops * N_ins);
-        gpu_copy(input, dinput);
+        assert((int)ops.size() == N_ops * N_ins);
+        dinkeys.resize(N_ops * N_ins);
+        dinvalues.resize(N_ops * N_ins);
+        dops.resize(N_ops * N_ins);
+        gpu_copy(ops, dops);
+        gpu_copy(inkeys, dinkeys);
+        gpu_copy(invals, dinvalues);
     }
     
     void procOps(vector<float> &ans){
         ans.resize(N_ins * N_ops);
         def_dvec(float) dans(N_ins * N_ops);
-        cudaProcKernel<<<N_ins, BLOCK_SIZE>>>(N_ins, to_ptr(dsizes), to_ptr(dkeys), to_ptr(dvalues), N_ops, to_ptr(dinput), to_ptr(dans));
+        cudaProcKernel<<<N_ins, BLOCK_SIZE>>>(N_ins, to_ptr(dsizes), to_ptr(dkeys), to_ptr(dvalues), N_ops, 
+        to_ptr(dops), to_ptr(dinkeys), to_ptr(dinvalues), to_ptr(dans));
         gpu_copy(dans, ans);
         return ;
     }
 };
 
 int main(int argc, char *argv[]){
-    int size = MAX_MAP_SIZE, num_ins = NUM_INSTANCE, num_ops = NUM_OPERATION;
+    srand(0);
+    int num_ins = NUM_INSTANCE, num_ops = NUM_OPERATION;
     if(argc > 1) num_ins = stoi(argv[1]);
     if(argc > 2) num_ops = stoi(argv[2]);
     /* using cudaEvent to evaluate time */
@@ -91,29 +164,22 @@ int main(int argc, char *argv[]){
     
     /* Generating data*/
     cudaEventRecord(start, 0);
-    vector<vector<int>> keys(num_ins);
-    vector<vector<float>> values(num_ins);
-    vector<int> input(num_ins * num_ops);
-    for(int i=0;i<num_ins;++i){
-        unordered_set<int> exist;
-        for(int j=0;j<size;++j){
-            int tmp_key = rand()%MOD;
-            if(!exist.count(tmp_key)){
-                keys[i].push_back(tmp_key);
-                values[i].push_back(float(rand())/RAND_MAX);
-                exist.insert(tmp_key);
-            }
-        }
-    }
-    generate(input.begin(), input.end(), [](){return rand()%MOD;});
+    string ref;
+    ref += string(500, 'g') + string(500, 's') + string(200, 'i') + string(200, 'm') + "e" + "zz" + "f" + string(100, 'r');
+    vector<char> ops(num_ins * num_ops);
+    vector<int> input_keys(num_ins * num_ops);
+    vector<float> input_values(num_ins * num_ops);
+    generate(input_keys.begin(), input_keys.end(), [](){return rand()%MOD;});
+    generate(input_values.begin(), input_values.end(), [](){return float(rand())/RAND_MAX;});
+    generate(ops.begin(), ops.end(), [&ref](){return ref[rand()%(int)ref.size()];});
     cudaEventRecord(stop, 0);                  // Stop time measuring
     cudaEventSynchronize(stop);
     cudaEventElapsedTime(&cuda_time, start, stop); // Saving the time measured
     cout<<"Time Usage for generating random data is: "<<cuda_time/1000<<"s"<<endl;
     
     cudaEventRecord(start, 0);
-    GPUMapTest gpu_test(keys, values);
-    gpu_test.loadOps(input, num_ops);
+    GPUMapTest gpu_test(num_ins);
+    gpu_test.loadOps(ops, input_keys, input_values, num_ops);
     cudaEventRecord(stop, 0);                  // Stop time measuring
     cudaEventSynchronize(stop);
     cudaEventElapsedTime(&cuda_time, start, stop); // Saving the time measured
