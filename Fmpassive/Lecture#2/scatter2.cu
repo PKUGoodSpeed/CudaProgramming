6548118f3373ffadc1ec5e46f993638699cdafd5
Zebo Li <zebo@ginkgo-llc.com>
2017-11-08 13:56:38 -0800

    renaming

--- Fmpassive/Lecture#2/scatter2.cu
+++ Fmpassive/Lecture#2/scatter2.cu
@@ -15,8 +15,9 @@ __global__ void init(){}
 __global__ void scatterSum(int N, float *input, float *output){
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     if(i >= N) return;
+    float a = input[i];
     for(int j=0;j<N;++j){
-        atomicAdd(output+(j+i)%N, input[i]);
+        atomicAdd(output+(j+i)%N, a);
     }
     return;
 }
